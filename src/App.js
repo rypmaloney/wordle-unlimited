3ce9eda1a592ed16f7eda2be642c17b965358b9c
Ryan Maloney <rypmaloney@gmail.com>
2022-01-25 12:49:35 -0500

    initial build with create-react-app.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+        <p>
+          Ready to go
+        </p>
+
+
+    </div>
+  );
+}
+
+export default App;
